f54a29cceb326276fc127f5fed3aead95e20d23d
Bryce Payne <brycedpayne@gmail.com>
2020-04-09 14:45:34 -0500

    added bonuses

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student {
 
+    private static Scanner scanner = new Scanner(System.in);
     private static long counter;
     public String name;
     public long id;
     public ArrayList<Integer> grades;
 
     public Student() {
-        counter ++;
+        counter++;
         this.name = name;
         this.id = id;
         this.grades = new ArrayList<>();
@@ -30,24 +32,50 @@ public class Student {
         this.id = id;
     }
 
-    public void addGrade(int grade){
+    public void addGrade(int grade) {
         grades.add(grade);
     }
 
     public double getGradeAverage() {
-        double sum =0;
-        for (int grade : grades){
+        double sum = 0;
+        for (int grade : grades) {
             sum += grade;
         }
-        return sum/grades.size();
+        return sum / grades.size();
     }
 
     public void updateGrade() {
+        int index = -1;
+        System.out.println(grades.toString());
+        System.out.print("Which grade would you like to change?: ");
+        int gradeToChange = scanner.nextInt();
+        System.out.print("What is the new grade?: ");
+        int newGrade = scanner.nextInt();
 
+        for (int grade : grades) {
+            index++;
+            if (gradeToChange == grade) {
+                grades.set(index,newGrade);
+                System.out.println("Grade was changed to " + grade);
+                break;
+            }
+        }
+        System.out.println(grades);
     }
 
     public void deleteGrade() {
+        System.out.println(grades.toString());
+        System.out.print("Which grade would you like to remove?: ");
+        int gradeToRemove = scanner.nextInt();
 
+        for (int grade : grades) {
+            if (gradeToRemove == grade) {
+                grades.remove((Integer) grade);
+                System.out.println("Grade was removed");
+                break;
+            }
+        }
+        System.out.println(grades);
     }
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -11,6 +11,11 @@ public class StudentTest {
     public void createStudent() {
         newStudent = new Student();
         newStudent.setName("Bryce");
+        newStudent.addGrade(95);
+        newStudent.addGrade(95);
+        newStudent.addGrade(95);
+        newStudent.addGrade(95);
+        newStudent.addGrade(95);
     }
 
     @Test
@@ -18,5 +23,12 @@ public class StudentTest {
         assertNotNull(newStudent.name);
         assertNotNull(newStudent.id);
         assertNotNull(newStudent.grades);
+    }
+
+    @Test
+    public void testGradeAverage() {
+        assertEquals(newStudent.getGradeAverage(),95,0.0);
+        assertNotEquals(newStudent.getGradeAverage(),94,0.9);
+        assertNotEquals(newStudent.getGradeAverage(),96,0.9);
     }
 }
